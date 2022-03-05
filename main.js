ed919b15178dba0538fc8c8ffb9121e265ed9925
KristyMik <mikulcova.kristyna@gmail.com>
2022-03-05 10:31:36 +0100

    Pohyb i obrázky

--- main.js
+++ main.js
@@ -20,40 +20,41 @@ console.log(window.getComputedStyle(panacek).getPropertyValue("bottom"));
 
 function pohniPanackem(x) {
 
-	
 	x = x || window.event;
 	
-
 	if(x.keyCode == '37') {
 		//leva sipka
 		let vlevo = parseInt(window.getComputedStyle(panacek).getPropertyValue("left"));
 		panacek.style.left = vlevo - 20 + "px";
 		console.log("leva sipka");
 		console.log(window.getComputedStyle(panacek).getPropertyValue("left"));
+		panacek.src = "obrazky/panacek-vlevo.png";
 	}
+
 	else if(x.keyCode == '38') {
 		//sipka nahoru 
-		console.log("sipka nahoru");
 		let nahore = parseInt(window.getComputedStyle(panacek).getPropertyValue("top"));
-		
+		console.log("sipka nahoru");
+		panacek.src = "obrazky/panacek-nahoru.png";
 		panacek.style.top = nahore - 20 + "px";
 			}
+
 			else if(x.keyCode == '39') {
 				//prava sipka
-				console.log("prava sipka");
-				let vlevo= parseInt(window.getComputedStyle(panacek).getPropertyValue("left"));
-		
-				panacek.style.left = vlevo + 20 + "px";
-
+				let vpravo= parseInt(window.getComputedStyle(panacek).getPropertyValue("left"));
+		    	console.log("prava sipka");
+				panacek.style.left = vpravo + 20 + "px";
+				panacek.src = "obrazky/panacek-vpravo.png";
 					}
+
 					else if(x.keyCode == '40') {
-						let nahore = parseInt(window.getComputedStyle(panacek).getPropertyValue("top"));
 						//sipka dolu 
+						let nahore = parseInt(window.getComputedStyle(panacek).getPropertyValue("top"));
 						panacek.style.top = nahore + 20 + "px";
-						console.log(nahore + 5 + "px");
 						console.log("sipka dolu");
+						panacek.src = "obrazky/panacek.png";
 							}
-														
+
 
 }
 
